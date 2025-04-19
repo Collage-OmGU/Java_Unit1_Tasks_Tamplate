53468ef97dff12db2966bd1d6516c7037ab7fcd4
Артём Баранов <artembaranove2001@gmail.com>
2025-04-19 13:50:06 +0600

    Структура и тест

---
+++ src/main/StudentTasks.java
@@ -0,0 +1,204 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+
+public class StudentTasks {
+
+    // Задание 1: Вывод числа, введенного пользователем
+    public static class Task1 {
+        public static void printNumber() {
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.print("Введите число: ");
+                    int number = scanner.nextInt();
+                    System.out.println("Вы ввели: " + number);
+                }
+    }
+
+    // Задание 2: Сумма двух чисел
+    public static class Task2 {
+        public static void sumOfTwoNumbers() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 3: Среднее арифметическое трех чисел
+    public static class Task3 {
+        public static void averageOfThreeNumbers() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 4: Площадь прямоугольника
+    public static class Task4 {
+        public static void rectangleArea() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 5: Проверка на четность
+    public static class Task5 {
+        public static void checkEvenOrOdd() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 6: Факториал числа
+    public static class Task6 {
+        public static void factorial() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 7: Проверка на простое число
+    public static class Task7 {
+        public static void checkPrime() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 8: Таблица умножения
+    public static class Task8 {
+        public static void multiplicationTable() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 9: Бинарное представление числа
+    public static class Task9 {
+        public static void binaryRepresentation() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 10: Максимальное из трех чисел
+    public static class Task10 {
+        public static void maxOfThreeNumbers() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 11: Сумма чисел от 1 до N
+    public static class Task11 {
+        public static void sumUpToN() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 12: Проверка на палиндром
+    public static class Task12 {
+        public static void checkPalindrome() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 13: Квадратный корень числа
+    public static class Task13 {
+        public static void squareRoot() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 14: Сумма цифр числа
+    public static class Task14 {
+        public static void sumOfDigits() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 15: Переворот числа
+    public static class Task15 {
+        public static void reverseNumber() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 16: Проверка на совершенное число
+    public static class Task16 {
+        public static void checkPerfectNumber() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 17: НОД двух чисел
+    public static class Task17 {
+        public static void gcd() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 18: Минимальное из двух чисел
+    public static class Task18 {
+        public static void minOfTwoNumbers() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 19: Изменение знака числа
+    public static class Task19 {
+        public static void changeSign() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 20: Угадай число (случайное от 1 до 100)
+    public static class Task20 {
+        public static void guessNumber() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 21: Умножение матриц
+    public static class Task21 {
+        public static void matrixMultiplication() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 22: Числа Фибоначчи
+    public static class Task22 {
+        public static void fibonacciSequence() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 23: Сортировка массива
+    public static class Task23 {
+        public static void sortArray() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 24: Сумма арифметической прогрессии
+    public static class Task24 {
+        public static void arithmeticProgressionSum() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 25: Угадай число с 10 попытками
+    public static class Task25 {
+        public static void guessNumberWithAttempts() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 26: Количество четных и нечетных цифр
+    public static class Task26 {
+        public static void countEvenAndOddDigits() {
+            // Реализация здесь
+        }
+    }
+
+    // Задание 27: Циклический поворот массива
+    public static class Task27 {
+        public static void rotateArray() {
+            // Реализация здесь
+        }
+    }
+
+    // Главный метод для тестирования (можно удалить в финальной версии)
+    public static void main(String[] args) {
+        // Пример вызова одного из заданий для проверки
+        Task1.printNumber();
+    }
+}
